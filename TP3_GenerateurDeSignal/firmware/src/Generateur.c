4c457051aea4b78d8c06489dbb69d5e1d81287f5
Perret <Melissa.perret@eduvaud.ch>
2024-03-20 21:46:36 +0100

    Version corrigé finale

--- TP3_GenerateurDeSignal/firmware/src/Generateur.c
+++ TP3_GenerateurDeSignal/firmware/src/Generateur.c
@@ -16,18 +16,6 @@
 #include "Mc32DriverLcd.h"
 
 
-// Tableau des échantillons de sinus
-const uint16_t tableauCourbeSinus[100] = {
- 53, 56, 59, 62, 65, 68, 71, 74, 77, 79,
- 82, 84, 86, 89, 90, 92, 94, 95, 96, 98,
- 98, 99, 100, 100, 100, 100, 100, 99, 98,
- 98, 96, 95, 94, 92, 90, 89, 86, 84, 82,
- 79, 77, 74, 71, 68, 65, 62, 59, 56, 53,
- 50, 47, 44, 41, 38, 35, 32, 29, 26, 23,
- 21, 18, 16, 14, 11, 10, 8, 6, 5, 4, 2,
- 2, 1, 0, 0, 0, 0, 0, 1, 2, 2, 4, 5, 6,
- 8, 10, 11, 14, 16, 18, 21, 23, 26, 29,
- 32, 35, 38, 41, 44, 47, 50};
 
 // Variables globales
 S_ParamGen valeursParamGen;  //Structure intermédiaire des paramètres du générateur
@@ -98,9 +86,23 @@ void  GENSIG_UpdatePeriode(S_ParamGen *pParam)
 void    GENSIG_UpdateSignal(S_ParamGen *pParam)
 {
     uint8_t nbEchantillon;
-    int16_t amplitude;
-    amplitude = pParam ->Amplitude / 100;
+    uint16_t amplitude;
     int16_t offset;
+   
+    // Tableau des échantillons de sinus
+    const uint16_t tableauCourbeSinus[100] = {
+     53, 56, 59, 62, 65, 68, 71, 74, 77, 79,
+     82, 84, 86, 89, 90, 92, 94, 95, 96, 98,
+     98, 99, 100, 100, 100, 100, 100, 99, 98,
+     98, 96, 95, 94, 92, 90, 89, 86, 84, 82,
+     79, 77, 74, 71, 68, 65, 62, 59, 56, 53,
+     50, 47, 44, 41, 38, 35, 32, 29, 26, 23,
+     21, 18, 16, 14, 11, 10, 8, 6, 5, 4, 2,
+     2, 1, 0, 0, 0, 0, 0, 1, 2, 2, 4, 5, 6,
+     8, 10, 11, 14, 16, 18, 21, 23, 26, 29,
+     32, 35, 38, 41, 44, 47, 50};
+
+    amplitude = pParam ->Amplitude / 100;
     offset = pParam->Offset * -0.5;//on fait fois -1 car le - est pour inverser le signal. Sinon le sinus monte quand on mettait -5000 au lieu de descendre 
     
     // Boucle pour le calcul de tous les échantillons
